7f555840b71d4b3158dbe61eba15cc809ef8f589
juergen.lesmeister <juergen.lesmeister@avid.com>
2017-12-21 16:22:04 +0100

    Added second Gradle example: managing a dependency

---
+++ GradleExample2/src/main/Main.java
@@ -0,0 +1,11 @@
+package com.jel.GradleExample;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println(StringUtils.swapCase("Hello world!"));
+	}
+	
+}
